5bc078a3203647c99104de58dc4a0ef377babe7e
Kirill Bobykin <qelphybox@gmail.com>
2020-12-27 20:29:26 +0300

    fix docs and schemas

--- lib/pornhub_api.rb
+++ lib/pornhub_api.rb
@@ -27,6 +27,8 @@ module PornhubApi
     #
     # ==== Example Request video search
     # PornhubApi.search # =>
+    # {
+    #     "videos": [
     #         {
     #             "duration": "11:20",
     #             "views": 2597,
@@ -57,6 +59,8 @@ module PornhubApi
     #             ],
     #             "segment": "straight"
     #         },
+    #     ]
+    # }
     def search(**options)
       webmasters_request("search", options)
     end
@@ -106,26 +110,99 @@ module PornhubApi
     # @param [Hash] options
     # @option options [String] thumbsize Possible values are small, medium, large, small_hd, medium_hd, large_hd
     # @return [Object] response
+    #
+    # ==== Example
+    #
+    # PornhubApi.video_by_id(id: 'ph5f59beb3df90a') # =>
+    # {
+    #   "video": {
+    #     "duration": "0:04",
+    #     "views": 2,
+    #     "video_id": "ph5f59beb3df90a",
+    #     "rating": 0,
+    #     "ratings": 0,
+    #     "title": "Dick stroke",
+    #     "url": "https://www.pornhub.com/view_video.php?viewkey=ph5f59beb3df90a",
+    #     "default_thumb": "https://ei.phncdn.com/videos/202009/10/350564541/original/(m=eaf8Ggaaaa)(mh=S0b3ophAz8vJXP8c)12.jpg",
+    #     "thumb": "https://ei.phncdn.com/videos/202009/10/350564541/original/(m=eaf8Ggaaaa)(mh=S0b3ophAz8vJXP8c)13.jpg",
+    #     "publish_date": "2020-12-26 22:44:47",
+    #     "thumbs": [
+    #       {
+    #         "size": "320x240",
+    #         "width": "320",
+    #         "height": "240",
+    #         "src": "https://ei.phncdn.com/videos/202009/10/350564541/original/(m=eaf8Ggaaaa)(mh=S0b3ophAz8vJXP8c)15.jpg"
+    #       }
+    #     ],
+    #     "tags": [
+    #       {
+    #         "tag_name": "big cock"
+    #       }
+    #     ],
+    #     "pornstars": [],
+    #     "categories": [
+    #       {
+    #         "category": "amateur"
+    #       }
+    #     ],
+    #     "segment": "straight"
+    #   }
+    # }
     def video_by_id(id:, **options)
       webmasters_request("video_by_id", { id: id, **options })
     end
 
+    # Get video activeness
     # @param [Object] id Video id
     # @param [Hash] options
     # @return [Object] response
     # rubocop:disable Naming/PredicateName
+    #
+    # ==== Example
+    # PornhubApi.is_video_active(id: 'ph5f59beb3df90a') # =>
+    # {
+    #     "active": {
+    #         "video_id": "ph5f59beb3df90a",
+    #         "is_active": "1"
+    #     }
+    # }
     def is_video_active(id:, **options)
       webmasters_request("is_video_active", { id: id, **options })
     end
     # rubocop:enable Naming/PredicateName
 
+    # Get all categories
     # @return [Object] response
+    # PornhubApi.categories # =>
+    # {
+    #   "categories": [
+    #     {
+    #       "id": "139",
+    #       "category": "verified-models"
+    #     }
+    #   ]
+    # }
     def categories
       webmasters_request("categories")
     end
 
+    # Get all tags by first char
     # @param [Object] list
     # @return [Object] response
+    # PornhubApi.tags(list: 'a') # =>
+    # {
+    #   "warning": "We had to change the response structure due to high amount of tags. Please adjust your code",
+    #   "tagsCount": 283196,
+    #   "tags": [
+    #     "płąnący",
+    #     "płaska",
+    #     "płaszcz",
+    #     "płaszczyk",
+    #     "pławska",
+    #     "płock",
+    #     "płonę"
+    #   ]
+    # }
     def tags(list:)
       webmasters_request("tags", { list: list })
     end
